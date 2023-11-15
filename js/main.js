232f653bfb2023660bffc9e4ad60ffdefbbc40b8
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-15 16:20:43 +0100

    started the backbone of the exercise

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// che informazioni ho ?
+// so che devo stampare dei numeri da 1 a 100
+// per ogni multiplo di 3 devo far stampare fizz
+//per i multipli di 5 devo far stampare buzz
+// per  i multipli di entrambi far stampare fizz buzz
+
+// stampo numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log("1")
+}
+
+//devo far stamapre per i multipli di 3 fizz come faccio ?
+
+
+
+// devo far stampare per i multipli di 5 buzz come faccio?
+
+
+
+// stampare per i multipli di 3 e 5 fizzbuzz 
+
+
